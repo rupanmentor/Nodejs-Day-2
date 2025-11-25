1e00e392d028537321a8ebc122364da2fe1b888f
rupanmentor <rupanguvimentor@gmail.com>
2025-11-25 19:05:12 +0530

    base code

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+//dotenv config
+
+dotenv.config();
+
+//express initialization
+
+const app = express();
+
+//default middleware
+
+app.use(express.json());
+
+app.use(cors());
+
+//default route
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to Backend");
+});
+
+//port
+
+const port = process.env.PORT || 5000;
+
+//server starting
+
+app.listen(port, () => {
+  console.log("server started");
+});
